a09f9a862542ad41c9866860363eff9ddae877e0
Antti Leppä <antti.leppa@foyt.fi>
2014-05-15 11:48:58 +0300

    ** Added progress indicator support into CloneGitRepositoryPhase

--- muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/CloneGitRepositoryPhase.java
+++ muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/CloneGitRepositoryPhase.java
@@ -7,14 +7,23 @@ public class CloneGitRepositoryPhase extends InstallerPhase {
   private static final String REPOSITORY_URL = "https://github.com/otavanopisto/muikku.git";
   
   @Override
+  public String getName() {
+    return "Import Projects";
+  }
+  
+  @Override
   public void execute(InstallerContext context) throws Exception {
-    // TODO: Configurable source folder
     File baseFolder = getBaseFolder(context);
     File sourceFolder = new File(baseFolder, "muikku");
     
-    runCommand(baseFolder, "git", "clone", REPOSITORY_URL, sourceFolder.getAbsolutePath());
-    runCommand(sourceFolder, "git", "checkout", "--track", "origin/devel");
-    context.setOption(InstallerContext.SOURCE_FOLDER, sourceFolder.getAbsolutePath());
+    String taskId = startTask("Cloning git repository");
+    try {
+      runCommand(baseFolder, "git", "clone", REPOSITORY_URL, sourceFolder.getAbsolutePath());
+      runCommand(sourceFolder, "git", "checkout", "--track", "origin/devel");
+      context.setOption(InstallerContext.SOURCE_FOLDER, sourceFolder.getAbsolutePath());
+    } finally {
+      endTask(taskId); 
+    }
   }
 
 }
