6615c107f57f750eaeb322db703104f5b3bdb989
Heikki Kurhinen <heikki.kurhinen@otavanopisto.fi>
2014-05-19 12:12:08 +0300

    ** Installer now accepts commanline parameters

--- muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/AssignOptionsPhase.java
+++ muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/AssignOptionsPhase.java
@@ -4,29 +4,28 @@ import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 
-
 public class AssignOptionsPhase extends InstallerPhase {
 
   @Override
   public String getName() {
     return "Options";
   }
-  
+
   @Override
   public void execute(InstallerContext context) throws Exception {
-    context.setOption(InstallerContext.BASEDIR, "/tmp/e/");
-    context.setOption(InstallerContext.ECLIPSE_FOLDER, "/tmp/e/eclipse");
-    context.setOption(InstallerContext.ECLIPSE_WORKSPACE_FOLDER, "/tmp/e/workspace");
-    context.setOption(InstallerContext.SOURCE_FOLDER, "/tmp/e/muikku");
-    context.setOption(InstallerContext.JBOSS_FOLDER, "/tmp/e/jboss");
-    
     File baseDir = context.getFileOption(InstallerContext.BASEDIR);
+    String fileSeparator = File.separator;
+
+    context.setOption(InstallerContext.ECLIPSE_FOLDER, baseDir.getAbsolutePath() + fileSeparator + "eclipse");
+    context.setOption(InstallerContext.ECLIPSE_WORKSPACE_FOLDER, baseDir.getAbsolutePath() + fileSeparator + "workspace");
+    context.setOption(InstallerContext.SOURCE_FOLDER, baseDir.getAbsolutePath() + fileSeparator + "muikku");
+    context.setOption(InstallerContext.JBOSS_FOLDER, baseDir.getAbsolutePath() + fileSeparator + "jboss");
+
     if (baseDir.exists()) {
       FileUtils.deleteDirectory(baseDir);
     }
-    
+
     baseDir.mkdirs();
   }
-  
 
 }
--- muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/Main.java
+++ muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/Main.java
@@ -1,28 +1,92 @@
 package fi.otavanopisto.devtools.muikkuinstaller;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 import org.apache.commons.lang3.StringUtils;
 
 public class Main {
-  
+
+  public static final String INSTALLER_EXECUTABLE = "java -jar muikku-devenv-installer.jar";
+
   public static void main(String[] args) {
     try {
-      executePhases(
-        new AssignOptionsPhase(),
-        new InstallJBossPhase(),
-        new InstallEclipsePhase(),
-        new InstallEclipsePluginsPhase(),
-        new ConfigureEclipsePhase(),
-        new CloneGitRepositoryPhase(),
-        new ImportEclipseProjectsPhase()
-      );
+
+      ArrayList<InstallerPhase> phases = new ArrayList<InstallerPhase>();
+
+      CommandLineParser parser = new GnuParser();
+      Options options = new Options();
+      options.addOption("e", false, "install Eclipse");
+      options.addOption("E", false, "install required plugins for Eclipse");
+      options.addOption("j", false, "install JBoss AS");
+      options.addOption("J", false, "configure JBoss AS");
+      options.addOption("D", false, "drop and create MySQL/MariaDB database and user (requires mysql)");
+      options.addOption("r", false, "clone Muikku git repository (requires git)");
+      options.addOption("i", false, "import muikku projects into Eclipse");
+      options.addOption("a", false, "install and configure all");
+      options.addOption("h", "help", false, "print this message");
+
+      CommandLine cmd = parser.parse(options, args);
+
+      if (cmd.hasOption('h') || cmd.hasOption("help")) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(INSTALLER_EXECUTABLE + " [options] basedir", options);
+        System.exit(0);
+      }
+      if (cmd.hasOption('a')) {
+        phases.add(new AssignOptionsPhase());
+        phases.add(new InstallJBossPhase());
+        phases.add(new InstallEclipsePhase());
+        phases.add(new InstallEclipsePluginsPhase());
+        phases.add(new ConfigureEclipsePhase());
+        phases.add(new CloneGitRepositoryPhase());
+        phases.add(new ImportEclipseProjectsPhase());
+      } else {
+        phases.add(new AssignOptionsPhase());
+        if (cmd.hasOption('e')) {
+          phases.add(new InstallEclipsePhase());
+        }
+        if (cmd.hasOption('E')) {
+          phases.add(new InstallEclipsePluginsPhase());
+        }
+        if (cmd.hasOption('j')) {
+          phases.add(new InstallJBossPhase());
+        }
+        if (cmd.hasOption('J')) {
+          throw new UnsupportedOperationException("Not supported yet");
+        }
+        if (cmd.hasOption('D')) {
+          throw new UnsupportedOperationException("Not supported yet");
+        }
+        if (cmd.hasOption('r')) {
+          phases.add(new CloneGitRepositoryPhase());
+        }
+        if (cmd.hasOption('i')) {
+          phases.add(new ImportEclipseProjectsPhase());
+        }
+      }
+
+      if (cmd.getArgList().size() > 0) {
+        File basedir = new File(cmd.getArgList().get(0).toString());
+        executePhases(phases, basedir);
+      } else {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(INSTALLER_EXECUTABLE + " [options] basedir", options);
+      }
+
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  private static void executePhases(InstallerPhase... phases) throws Exception {
+  private static void executePhases(ArrayList<InstallerPhase> phases, File basedir) throws Exception {
     InstallerContext installerContext = new InstallerContext();
-
+    installerContext.setOption(InstallerContext.BASEDIR, basedir.getAbsolutePath());
     for (InstallerPhase phase : phases) {
       String line = StringUtils.repeat('-', phase.getName().length() + 4);
       System.out.println('\n' + line + "\n  " + phase.getName() + "\n" + line + '\n');
