ccb1211f228913a958ea56abea97be028260df12
Antti Leppä <antti.leppa@foyt.fi>
2014-05-19 08:18:08 +0300

    ** Added support for changing jboss vm args into muikku-devenv-installer

--- muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/ConfigureEclipsePhase.java
+++ muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/ConfigureEclipsePhase.java
@@ -19,13 +19,14 @@ public class ConfigureEclipsePhase extends AbstractEclipseConfigurationPhase {
     File eclipseWorkspaceFolder = getEclipseWorkspaceFolder(context, true);
     File eclipseDropinsFolder = getEclipseDropinsFolder(eclipseFolder);
     File eclipseExecutable = getEclipseExecutable(context, eclipseFolder);
+    File sourceFolder = getSourceFolder(context);
     File jbossHome = getJBossHome(context);
     
     File yellowSheepJar = installYellowSheep(eclipseDropinsFolder);
     try {
       importPreferences(context, eclipseFolder, eclipseExecutable, eclipseWorkspaceFolder);
       setMavenAnnotationProcessing(context, eclipseFolder, eclipseExecutable, eclipseWorkspaceFolder);
-      createServer(context, eclipseFolder, eclipseExecutable, jbossHome, eclipseWorkspaceFolder);
+      createServer(context, eclipseFolder, eclipseExecutable, jbossHome, eclipseWorkspaceFolder, sourceFolder);
     } finally {
       yellowSheepJar.delete();
     }
@@ -67,21 +68,38 @@ public class ConfigureEclipsePhase extends AbstractEclipseConfigurationPhase {
     }
   }
   
-  private void createServer(InstallerContext context, File eclipseFolder, File eclipseExecutable, File jbossHome, File eclipseWorkspaceFolder) throws IOException, InterruptedException {
+  private void createServer(InstallerContext context, File eclipseFolder, File eclipseExecutable, File jbossHome, File eclipseWorkspaceFolder, File sourceFolder) throws IOException, InterruptedException {
     String taskId = startTask("Adding JBoss AS 7.1 server");
     try {
-      List<String> arguments = new ArrayList<String>();
-      arguments.add("-nosplash");
-      arguments.add("-application");
-      arguments.add("yellow-sheep-project.yellow-sheep-project");
-      arguments.add("-data");
-      arguments.add(eclipseWorkspaceFolder.getAbsolutePath());
-      arguments.add("-configure-jbossas71");
-      arguments.add(jbossHome.getAbsolutePath());
-      runEclipse(context, eclipseFolder, eclipseExecutable, arguments);
+      configureJBossAS71(context, eclipseFolder, eclipseExecutable, jbossHome, eclipseWorkspaceFolder);
+      addWorkspaceVMArg(context, eclipseFolder, eclipseExecutable, jbossHome, eclipseWorkspaceFolder, sourceFolder);
     } finally {
       endTask(taskId);
     }
+  }
+
+  private void configureJBossAS71(InstallerContext context, File eclipseFolder, File eclipseExecutable, File jbossHome, File eclipseWorkspaceFolder) throws IOException, InterruptedException {
+    List<String> arguments = new ArrayList<String>();
+    arguments.add("-nosplash");
+    arguments.add("-application");
+    arguments.add("yellow-sheep-project.yellow-sheep-project");
+    arguments.add("-data");
+    arguments.add(eclipseWorkspaceFolder.getAbsolutePath());
+    arguments.add("-configure-jbossas71");
+    arguments.add(jbossHome.getAbsolutePath());
+    runEclipse(context, eclipseFolder, eclipseExecutable, arguments);
+  }
+
+  private void addWorkspaceVMArg(InstallerContext context, File eclipseFolder, File eclipseExecutable, File jbossHome, File eclipseWorkspaceFolder, File sourceFolder) throws IOException, InterruptedException {
+    List<String> arguments = new ArrayList<String>();
+    arguments.add("-nosplash");
+    arguments.add("-application");
+    arguments.add("yellow-sheep-project.yellow-sheep-project");
+    arguments.add("-data");
+    arguments.add(eclipseWorkspaceFolder.getAbsolutePath());
+    arguments.add("-add-jbossas71-vmargs");
+    arguments.add("-Declipse.workspace=" + sourceFolder.getAbsolutePath() + "/muikku/");
+    runEclipse(context, eclipseFolder, eclipseExecutable, arguments);
   }
   
 }
