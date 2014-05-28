adc5849567a4dec80907dfe85d2547cf1e6761d6
Antti Leppä <antti.leppa@foyt.fi>
2014-05-28 13:08:38 +0300

    ** Removed dysfunctional maven update part of ImportEclipseProjectPhase

--- muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/ImportEclipseProjectsPhase.java
+++ muikku-devenv-installer/src/main/java/fi/otavanopisto/devtools/muikkuinstaller/ImportEclipseProjectsPhase.java
@@ -3,7 +3,6 @@ package fi.otavanopisto.devtools.muikkuinstaller;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -33,33 +32,29 @@ public class ImportEclipseProjectsPhase extends AbstractEclipseConfigurationPhas
   }
   
   protected void importProjects(InstallerContext context, File eclipseWorkspaceFolder, File eclipseFolder, File eclipseExecutable, File sourceFolder) throws IOException, InterruptedException {
-    LinkedHashMap<String, String> projects = new LinkedHashMap<String, String>();
-    projects.put("fi.muikku.bom", "muikku-bom/pom.xml");
-    projects.put("fi.muikku.parent", "pom.xml");      
-    projects.put("fi.muikku.security", "muikku-security/pom.xml"); 
-    projects.put("fi.muikku.persistence", "muikku-persistence/pom.xml");
-    projects.put("fi.muikku.plugin", "muikku-plugin/pom.xml");
-    projects.put("fi.muikku.plugin-core", "muikku-plugin-core/pom.xml");
-    projects.put("fi.muikku.rest", "muikku-rest/pom.xml");
-    projects.put("fi.muikku.core", "muikku-core/pom.xml ");
-    projects.put("fi.muikku.muikku", "muikku/pom.xml");
-    projects.put("fi.muikku.core-plugins", "muikku-core-plugins/pom.xml");
-    projects.put("fi.muikku.dummy-mail-bridge-plugin", "muikku-jndi-mail-bridge-plugin/pom.xml");
-    projects.put("fi.muikku.facebook-oauth-plugin", "muikku-facebook-oauth/pom.xml");
-    projects.put("fi.muikku.google-oauth-plugin", "muikku-google-oauth/pom.xml");
-    projects.put("fi.muikku.h2db-plugin", "muikku-h2db-plugin/pom.xml");
-    projects.put("fi.muikku.jndi-mail-bridge-plugin", "muikku-dummy-mail-bridge-plugin/pom.xml");
-    projects.put("fi.muikku.school-data-mock", "muikku-school-data-mock/pom.xml");
+    List<String> poms = new ArrayList<String>();
+    poms.add("muikku-bom/pom.xml");
+    poms.add("pom.xml");      
+    poms.add("muikku-security/pom.xml"); 
+    poms.add("muikku-persistence/pom.xml");
+    poms.add("muikku-plugin/pom.xml");
+    poms.add("muikku-plugin-core/pom.xml");
+    poms.add("muikku-rest/pom.xml");
+    poms.add("muikku-core/pom.xml ");
+    poms.add("muikku/pom.xml");
+    poms.add("muikku-core-plugins/pom.xml");
+    poms.add("muikku-jndi-mail-bridge-plugin/pom.xml");
+    poms.add("muikku-facebook-oauth/pom.xml");
+    poms.add("muikku-google-oauth/pom.xml");
+    poms.add("muikku-h2db-plugin/pom.xml");
+    poms.add("muikku-dummy-mail-bridge-plugin/pom.xml");
+    poms.add("muikku-school-data-mock/pom.xml");
     
-    for (String project : projects.keySet()) {
-      String taskId = startTask("Import project " + project);
-      try {
-        String pom = projects.get(project);
-        importProjects(context, eclipseWorkspaceFolder, eclipseFolder, eclipseExecutable, sourceFolder, pom);
-        updateProjects(context, eclipseWorkspaceFolder, eclipseFolder, eclipseExecutable, project);
-      } finally {
-        endTask(taskId);
-      }
+    String importTaskId = startTask("Importing projects to Eclipse");
+    try {
+      importProjects(context, eclipseWorkspaceFolder, eclipseFolder, eclipseExecutable, sourceFolder, poms.toArray(new String[0]));
+    } finally {
+      endTask(importTaskId);
     }
   }
 
@@ -80,18 +75,6 @@ public class ImportEclipseProjectsPhase extends AbstractEclipseConfigurationPhas
     runEclipse(context, eclipseFolder, eclipseExecutable, arguments);
   }
 
-  private void updateProjects(InstallerContext context, File eclipseWorkspaceFolder, File eclipseFolder, File eclipseExecutable, String... projects) throws IOException, InterruptedException {
-    List<String> arguments = new ArrayList<String>();
-    arguments.add("-nosplash");
-    arguments.add("-application");
-    arguments.add("yellow-sheep-project.yellow-sheep-project");
-    arguments.add("-data");
-    arguments.add(eclipseWorkspaceFolder.getAbsolutePath());
-    arguments.add("-update-projects");
-    arguments.add(StringUtils.join(projects, ','));
-    runEclipse(context, eclipseFolder, eclipseExecutable, arguments);
-  }
-  
   private void addWebAppToJBoss(InstallerContext context, File eclipseWorkspaceFolder, File eclipseFolder, File eclipseExecutable) throws IOException, InterruptedException {
     List<String> arguments = new ArrayList<String>();
     arguments.add("-nosplash");
