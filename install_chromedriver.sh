b2cd439606f6c0230fbdb5879bd5a42e5254b3ca
petja-laitila <petja.laitila@otavanopisto.fi>
2017-12-28 10:46:14 +0200

    ChromeDriver installer for linux

---
+++ install_chromedriver.sh
@@ -0,0 +1,9 @@
+wget https://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo cp chromedriver /usr/bin/chromedriver
+sudo chown root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
+sudo chmod 755 /usr/bin/chromedriver
+rm chromedriver_linux64.zip chromedriver
+echo "All Done!"
+
